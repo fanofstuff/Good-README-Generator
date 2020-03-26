1939b235a2bf8329c31c9a271c5d1af87fe40b4c
Tucker Hembree <bladeofxp@gmail.com>
2020-03-26 17:55:23 -0400

    created basic prompt asking for Github username and console logging the avatar_url; additionally, installed all packages through npm install

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
 
-];
+// return writeFileAsync("example.txt", result.data.avatar_url);
 
-function writeToFile(fileName, data) {
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: "username"
+    }
+  ]);
 }
 
-function init() {
-
+function queryGithub(data) {
+  const queryURL = `https://api.github.com/users/${data.username}`;
+  axios.get(queryURL).then(function(result) {
+    console.log(result.data.avatar_url);
+  });
 }
 
-init();
+promptUser()
+  .then(function(data) {
+    queryGithub(data);
+    // generateREADME(data);
+  })
+  .then(() => console.log("Successfully wrote to README.md"))
+  .catch(err => console.log(err));
+
+// const questions = [];
+
+// function writeToFile(fileName, data) {}
+
+// function init() {}
+
+// init();
